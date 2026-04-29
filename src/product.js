4e8118a33a36b2baa4522caedb3af6dd43d33d7a
Vullnet02 <vullnetjakupi3@gmail.com>
2026-04-29 16:11:11 +0200

    ndryshimi i fundit

--- src/product.js
+++ src/product.js
@@ -6,6 +6,7 @@ const products = [
   { id: 1, name: "Lenovo Laptop",     description: "Intel i7, 16GB RAM, 512GB SSD", img: OIP, saleprice: 1299.99 },
   { id: 2, name: "Iphone 17 Pro Max", description: "2.4GHz, 12 months battery", img: mobile, saleprice: 999.99 },
   { id: 3, name: "Electric Scooter", description: "Eco-friendly, 20km range, 25km/h top speed", img: Scooter, saleprice: 299.99 },
+  
 ];
 
 export default products;
